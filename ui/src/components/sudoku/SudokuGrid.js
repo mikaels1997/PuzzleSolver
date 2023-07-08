49ef3a811350a551730cb0f2c33cc66515c0373f
Mikael Särkiniemi <mikaels1997@gmail.com>
2023-07-08 23:54:52 +0300

    proper input handling

--- ui/src/components/sudoku/SudokuGrid.js
+++ ui/src/components/sudoku/SudokuGrid.js
@@ -7,10 +7,10 @@ const SudokuGrid = () => {
   return <div className="sudoku-main">
     {
       numArray.map(r => {
-      return <div className="boxrow">
+      return <div className="boxrow" key={r}>
+        <SudokuBox row={r} col={0}/>
         <SudokuBox row={r} col={1}/>
         <SudokuBox row={r} col={2}/>
-        <SudokuBox row={r} col={3}/>
       </div>})
     }
   </div>;
@@ -20,25 +20,36 @@ const SudokuBox = ({row, col}) => {
   const numArray = [...Array(SIZE/3).keys()];
   return <div className="sudokubox">
     {
-      numArray.map(c => {
-        return <div className="boxrow">
-          <SudokuCell digit={col}/>
-          <SudokuCell digit={col}/>
-          <SudokuCell digit={col}/>
+      numArray.map(r => {
+        return <div className="boxrow" key={r}>
+          <SudokuCell row={3*row+r} col={3*col}/>
+          <SudokuCell row={3*row+r}  col={3*col+1}/>
+          <SudokuCell row={3*row+r}  col={3*col+2}/>
         </div>})
     }
   </div>;
 }
 
-const SudokuCell = ({digit}) => {
+const SudokuCell = ({row, col}) => {
   return <input className="sudokucell" 
-    maxLength={1}
-    onInput={validateInput}>
+    maxLength={2}
+    onChange={changeInput}
+    onClick={handleClick}>
   </input>
 }
 
-function validateInput(input) {
-  input.target.value = input.target.value.replace(/[^0-9]/g, "")
+function changeInput(event) {
+  event.target.value = event.target.value.replace(/[^1-9]/g, "")
+  if (event.target.value == "") return;
+  const input = event.target;
+  const inputValue = event.target.value;
+  input.value = inputValue[inputValue.length - 1];
+}
+
+function handleClick(event) {
+  const {value} = event.target;
+  const position = value.length;
+  event.target.setSelectionRange(position, position);
 }
 
 export default SudokuGrid;
